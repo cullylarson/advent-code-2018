f3c77605da01ae2d0ac589cdc4817608f61bcd61
Cully Larson <cully.larson@gmail.com>
2019-01-09 19:13:08 -0800

    Finished day 8, part 2.

---
+++ day8-2/index.js
@@ -0,0 +1,95 @@
+const R = require('ramda')
+const {get, liftA} = require('@cullylarson/f')
+
+const input = require('./input')
+
+const getMetas = (nodeList, numMeta, startIdx) => {
+    return numMeta === 0
+        ? {
+            metas: [],
+            endIdx: startIdx,
+        }
+        : {
+            metas: R.slice(startIdx, startIdx + numMeta, nodeList),
+            endIdx: startIdx + numMeta - 1,
+        }
+}
+
+const getChildren = (nodeList, numChildren, startIdx) => {
+    let children = []
+    let nextChildIdx = startIdx
+
+    for(let i = 0; i < numChildren; i++) {
+        const childInfo = getNode(nextChildIdx, nodeList)
+        children.push(childInfo.node)
+        nextChildIdx = childInfo.endIdx + 1
+    }
+
+    return {
+        children,
+        endIdx: children.length ? nextChildIdx - 1 : startIdx,
+    }
+}
+
+const createNode = (metas, children) => ({
+    metas,
+    children,
+})
+
+const getNode = R.curry((startIdx, nodeList) => {
+    const numChildren = nodeList[startIdx]
+    const numMeta = nodeList[startIdx + 1]
+
+    const childrenInfo = getChildren(nodeList, numChildren, startIdx + 2)
+
+    const metasStartIdx = numChildren === 0
+        ? startIdx + 2
+        : childrenInfo.endIdx + 1
+
+    const metasInfo = getMetas(nodeList, numMeta, metasStartIdx)
+
+    return {
+        node: createNode(metasInfo.metas, childrenInfo.children),
+        endIdx: metasInfo.endIdx,
+    }
+})
+
+// the indexes start at 1
+const pickMulti = R.curry((idxs, xs) => {
+    return R.reduce((acc, idx) => {
+        if(idx <= xs.length) acc.push(xs[idx - 1])
+
+        return acc
+    }, [], idxs)
+})
+
+const sumAllByMetaAsIndex = node => {
+    return node.children.length === 0
+        ? R.sum(node.metas)
+        : R.compose(
+            R.sum,
+            R.map(sumAllByMetaAsIndex),
+            pickMulti(node.metas),
+        )(node.children)
+}
+
+function report(x) {
+    console.log(x)
+    return x
+}
+
+const reportM = R.curry((msg, x) => {
+    console.log(msg, x)
+    return x
+})
+
+R.compose(
+    report,
+    sumAllByMetaAsIndex,
+    get('node', null),
+    getNode(0),
+    R.map(x => parseInt(x)),
+    R.filter(x => !!x),
+    R.map(R.trim),
+    R.split(' '),
+)(input)
